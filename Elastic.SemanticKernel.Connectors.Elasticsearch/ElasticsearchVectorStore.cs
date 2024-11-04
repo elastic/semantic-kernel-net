13e5d8213062ddaf014f30f582aa2ef60ed489fd
Florian Bernd <florian.bernd@elastic.co>
2024-11-04 12:00:29 +0100

    Release preparation

--- Elastic.SemanticKernel.Connectors.Elasticsearch/ElasticsearchVectorStore.cs
+++ Elastic.SemanticKernel.Connectors.Elasticsearch/ElasticsearchVectorStore.cs
@@ -1,3 +1,7 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -8,7 +12,6 @@ using Elastic.Transport;
 
 using Microsoft.Extensions.VectorData;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Data;
 
 namespace Elastic.SemanticKernel.Connectors.Elasticsearch;
 
--- Elastic.SemanticKernel.Playground/Program.cs
+++ Elastic.SemanticKernel.Playground/Program.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 using Elastic.Clients.Elasticsearch;
 using Elastic.Transport;
 
 using Elastic.SemanticKernel.Connectors.Elasticsearch;
-using Microsoft.SemanticKernel.Data;
+
+using Microsoft.Extensions.VectorData;
 
 namespace Elastic.SemanticKernel.Playground;
 
@@ -14,7 +17,7 @@ internal class Program
     static async Task Main(string[] args)
     {
         using var settings = new ElasticsearchClientSettings(new Uri("https://primary.es.europe-west3.gcp.cloud.es.io"))
-            .Authentication(new BasicAuthentication("elastic", "dJGI1c9D87cFFZZRNsv4xfxM"))
+            .Authentication(new BasicAuthentication("elastic", "g7wuRWPrJF2yAzytvd9w18s8"))
             .DisableDirectStreaming()
             .EnableDebugMode(cd =>
             {
@@ -26,28 +29,64 @@ internal class Program
         var vectorStore = new ElasticsearchVectorStore(client);
 
         var collection = vectorStore.GetCollection<string, MyRecord>("sk");
+
         await collection.CreateCollectionIfNotExistsAsync();
+
+        await collection.UpsertAsync(new MyRecord
+        {
+            MyKey = "40",
+            Vec1 = new float[] { 2.1f, 2.0f, 2.2f },
+            Vec2 = new float[] { 1.2f, 1.1f, 1.3f },
+            Data1 = 1337,
+            Data2 = DateTimeKind.Utc,
+            Data3 = ["a", "b", "c"]
+        });
+
+        await collection.UpsertAsync(new MyRecord
+        {
+            MyKey = "41",
+            Vec1 = new float[] { 0.1f, 0.1f, 1000.1f },
+            Vec2 = new float[] { 1.2f, 1.1f, 1.3f },
+            Data1 = 1338,
+            Data2 = DateTimeKind.Utc,
+            Data3 = ["a", "abba", "c"]
+        });
 
         var id = await collection.UpsertAsync(new MyRecord
         {
-            Id = "42",
-            Vec1 = "bla",
+            MyKey = "42",
+            Vec1 = new float[] { 2.2f, 2.1f, 2.3f, 0.0f },
             Vec2 = new float[] { 1.2f, 1.1f, 1.3f },
             Data1 = 1337,
-            Data2 = DateTimeKind.Utc
+            Data2 = DateTimeKind.Utc,
+            Data3 = ["a", "b", "c"]
         });
 
-
         var record = await collection.GetAsync(id);
+
+        var search = await collection.VectorizedSearchAsync(new float[] { 2.2f, 2.1f, 2.3f }, new VectorSearchOptions
+        {
+            IncludeTotalCount = true,
+            Filter = new VectorSearchFilter(new FilterClause[]
+            {
+                new AnyTagEqualToFilterClause(nameof(MyRecord.Data3), "abba"),
+                new EqualToFilterClause(nameof(MyRecord.Data1), 1338) // TODO: Test char
+            })
+        });
+
         var genericCollection = vectorStore.GetCollection<string, VectorStoreGenericDataModel<string>>("sk", new VectorStoreRecordDefinition
         {
             Properties =
             [
-                new VectorStoreRecordKeyProperty(nameof(MyRecord.Id), typeof(string)),
-                new VectorStoreRecordVectorProperty(nameof(MyRecord.Vec1), typeof(string)),
+                new VectorStoreRecordKeyProperty(nameof(MyRecord.MyKey), typeof(string)),
+                new VectorStoreRecordVectorProperty(nameof(MyRecord.Vec1), typeof(ReadOnlyMemory<float>))
+                {
+                    StoragePropertyName = "xxx"
+                },
                 new VectorStoreRecordVectorProperty(nameof(MyRecord.Vec2), typeof(ReadOnlyMemory<float>)),
                 new VectorStoreRecordDataProperty(nameof(MyRecord.Data1), typeof(int)),
-                new VectorStoreRecordDataProperty(nameof(MyRecord.Data2), typeof(DateTimeKind))
+                new VectorStoreRecordDataProperty(nameof(MyRecord.Data2), typeof(DateTimeKind)),
+                new VectorStoreRecordDataProperty(nameof(MyRecord.Data3), typeof(string[]))
             ]
         });
 
@@ -67,17 +106,26 @@ internal class Program
 public sealed class MyRecord
 {
     [VectorStoreRecordKey]
-    public string? Id { get; init; }
+    public required string? MyKey { get; init; }
 
     [VectorStoreRecordVector(3)]
-    public string Vec1 { get; init; }
+    [JsonPropertyName("xxx")]
+    public required IReadOnlyCollection<float> Vec1 { get; init; }
 
     [VectorStoreRecordVector(3)]
-    public ReadOnlyMemory<float> Vec2 { get; init; }
+    public required ReadOnlyMemory<float> Vec2 { get; init; }
 
     [VectorStoreRecordData(IsFilterable = true, IsFullTextSearchable = true)]
-    public int Data1 { get; init; }
+    public required int Data1 { get; init; }
 
     [VectorStoreRecordData(IsFilterable = true)]
-    public DateTimeKind Data2 { get; init; }
+    public required DateTimeKind Data2 { get; init; }
+
+    [VectorStoreRecordData(IsFilterable = true)]
+
+#pragma warning disable CA1819
+
+    public required string[] Data3 { get; init; }
+
+#pragma warning restore CA1819
 }
